aaf322082801917d49f66e375e3fb26c4b6ff602
Hhhxdjejjd <99557090+Hhhxdjejjd@users.noreply.github.com>
2022-04-17 18:24:48 -0400

    Dikdkdkdkkdd
    Dkkdkdkd

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -5,3 +5,32 @@ import flixel.text.FlxText;
 import flixel.FlxG;
 import flixel.ui.FlxButton;
 import flixel.FlxState;
+
+class MenuState extends FlxState
+{
+  private var title:FlxText;
+  private var play:FlxButton;
+  
+  override function create()
+  {
+    title = new FlxText(50, 0, 0, "dodger", 18);
+    title.alignment = CENTER;
+    title.screenCenter(X);
+    add(title);
+    
+    play = new FlxButton(0, 0, "Play", clickPlay);
+    play.x = (FlxG.width / 2) - (play.width / 2);
+    play.y = (FlxG.height / 2);
+    add(play);
+    
+    super.create();
+  }
+  
+  private function clickPlay()
+  {
+    FlxG.camera.fade(FlxColor.BLACK, 0.33, false, () ->
+    {
+      FlxG.switchState(new PlayState());
+    });
+  } 
+}
