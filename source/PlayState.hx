0cf52e7524ffc66a2998a7fe3848ed49456f1a38
Hhhxdjejjd <99557090+Hhhxdjejjd@users.noreply.github.com>
2022-04-17 18:55:11 -0400

    Kd,dkdke
    D,s,dk,d,xkdk

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -8,7 +8,6 @@ class PlayState extends FlxState
 	override public function create()
 	{
 		super.create();
-		add(new FlxText("Hello World", 32).screenCenter());
 	}
 
 	override public function update(elapsed:Float)
